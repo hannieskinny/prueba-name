a80f6cba6825a14ed8c0888131df1b9c2c7535e6
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 17:19:58 -0500

    Add method create and strings params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@car]
+    else
+      flash[:alert] = "Comment has not been created."
+      render :template => "cars/show"
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
 end
