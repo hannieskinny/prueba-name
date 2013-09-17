ce45e44fbfbcf7502bd3a90dc4452d1c80a6e35b
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 18:53:08 -0500

    Add migration column state_id into comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917230711) do
+ActiveRecord::Schema.define(version: 20130917234854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130917230711) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "state_id"
   end
 
   create_table "states", force: true do |t|
--- spec/features/create_tracking_states.rb
+++ spec/features/create_tracking_states.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 feature "Creating tracking states" do
   before do
-    visit comments_path
+    visit new_comment_path
   end
 
   scenario "Creating a comment" do
     fill_in "comment_text", :with => "Added a comment!"
     click_button "create-comment"
     page.should have_content("Comment has been created.")
-      within("#comments") do
-        page.should have_content("Added a comment!")
-      end
   end
-  scenario "Creating an invalid comment" do
-      click_button "create-comment"
-      page.should have_content("Comment has not been created.")
+  
+  scenario "Creating States" do
+    fill_in "comment_text", :with => "This is a comment"
+    select "open", :from => "State"
+    click_button "create_comment"
+    page.should have_content("Comment has been created.")
+    within(".state") do
+      page.should have_content("open")
+    end  
   end 
 end
