56b95d200deefec4603426044e698f73840fd6f5
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 18:26:24 -0500

    Create method set comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   validates :text, :presence => true
   belongs_to :state
+  after_create :set_comment_state
+
+
+  def set_comment_state
+    self.comment.state = self.state
+    self.comment.save!
+  end
+
 end
